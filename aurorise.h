a77589164f75c1a1b224cd077be72fcdb1b1956d
LenaAgrest <lena.agrest@yandex.ru>
2025-04-06 13:51:27 +0300

    работа с внешним видом

--- aurorise.h
+++ aurorise.h
@@ -34,19 +34,25 @@ namespace QQ {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
-	private: System::Windows::Forms::Panel^ panel2;
-	private: System::Windows::Forms::Button^ exit;
 
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+
+
+
 	private: System::Windows::Forms::Panel^ panel1;
-	private: System::Windows::Forms::TextBox^ textBox2;
+
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Panel^ panel3;
+
+
+	private: System::Windows::Forms::Button^ exit;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::TextBox^ textBox2;
 
 
 
@@ -71,89 +77,112 @@ namespace QQ {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(aurorise::typeid));
-			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
-			this->panel2 = (gcnew System::Windows::Forms::Panel());
-			this->exit = (gcnew System::Windows::Forms::Button());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
 			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->tableLayoutPanel1->SuspendLayout();
-			this->panel2->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->exit = (gcnew System::Windows::Forms::Button());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// tableLayoutPanel1
-			// 
-			this->tableLayoutPanel1->ColumnCount = 2;
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				50)));
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				50)));
-			this->tableLayoutPanel1->Controls->Add(this->panel2, 1, 0);
-			this->tableLayoutPanel1->Controls->Add(this->panel1, 0, 0);
-			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
-			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
-			this->tableLayoutPanel1->RowCount = 1;
-			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(782, 553);
-			this->tableLayoutPanel1->TabIndex = 0;
-			this->tableLayoutPanel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &aurorise::tableLayoutPanel1_Paint);
-			// 
-			// panel2
-			// 
-			this->panel2->Controls->Add(this->exit);
-			this->panel2->Controls->Add(this->pictureBox1);
-			this->panel2->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->panel2->Location = System::Drawing::Point(394, 3);
-			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(385, 547);
-			this->panel2->TabIndex = 3;
-			// 
-			// exit
-			// 
-			this->exit->FlatAppearance->BorderSize = 0;
-			this->exit->FlatStyle = System::Windows::Forms::FlatStyle::System;
-			this->exit->Location = System::Drawing::Point(307, 3);
-			this->exit->Name = L"exit";
-			this->exit->Size = System::Drawing::Size(75, 23);
-			this->exit->TabIndex = 1;
-			this->exit->Text = L"X";
-			this->exit->UseVisualStyleBackColor = true;
-			this->exit->Click += gcnew System::EventHandler(this, &aurorise::exit_Click);
-			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(0, 0);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(385, 547);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->pictureBox1->TabIndex = 0;
-			this->pictureBox1->TabStop = false;
-			// 
 			// panel1
 			// 
-			this->panel1->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->panel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left));
+			this->panel1->BackColor = System::Drawing::Color::Transparent;
+			this->panel1->Controls->Add(this->panel2);
+			this->panel1->Controls->Add(this->textBox2);
+			this->panel1->Controls->Add(this->panel3);
 			this->panel1->Controls->Add(this->button3);
 			this->panel1->Controls->Add(this->button1);
-			this->panel1->Controls->Add(this->textBox2);
 			this->panel1->Controls->Add(this->label3);
 			this->panel1->Controls->Add(this->textBox1);
 			this->panel1->Controls->Add(this->label2);
 			this->panel1->Controls->Add(this->label1);
-			this->panel1->Location = System::Drawing::Point(29, 53);
+			this->panel1->Location = System::Drawing::Point(37, 45);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(333, 447);
+			this->panel1->Size = System::Drawing::Size(407, 447);
 			this->panel1->TabIndex = 4;
+			// 
+			// panel3
+			// 
+			this->panel3->BackColor = System::Drawing::Color::SlateBlue;
+			this->panel3->Location = System::Drawing::Point(10, 229);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(361, 2);
+			this->panel3->TabIndex = 7;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::SlateBlue;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Montserrat SemiBold", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->ForeColor = System::Drawing::Color::White;
+			this->button3->Location = System::Drawing::Point(190, 354);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(181, 43);
+			this->button3->TabIndex = 4;
+			this->button3->Text = L"Нет аккаунта\?";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::SlateBlue;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Montserrat SemiBold", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(10, 354);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(161, 43);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Войти";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Montserrat SemiBold", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->ForeColor = System::Drawing::Color::SlateBlue;
+			this->label3->Location = System::Drawing::Point(3, 249);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(128, 39);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Пароль";
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::Color::White;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Montserrat Medium", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->ForeColor = System::Drawing::Color::MediumSlateBlue;
+			this->textBox1->Location = System::Drawing::Point(10, 197);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(361, 29);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Montserrat SemiBold", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->ForeColor = System::Drawing::Color::SlateBlue;
+			this->label2->Location = System::Drawing::Point(3, 150);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(80, 39);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Имя";
 			// 
 			// label1
 			// 
@@ -167,82 +196,65 @@ namespace QQ {
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Вход";
 			// 
-			// label2
+			// exit
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Montserrat SemiBold", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->exit->AutoSize = true;
+			this->exit->BackColor = System::Drawing::Color::Transparent;
+			this->exit->FlatAppearance->BorderSize = 0;
+			this->exit->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->exit->Font = (gcnew System::Drawing::Font(L"Montserrat SemiBold", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->ForeColor = System::Drawing::Color::SlateBlue;
-			this->label2->Location = System::Drawing::Point(3, 149);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(110, 39);
-			this->label2->TabIndex = 1;
-			this->label2->Text = L"Логин";
+			this->exit->Location = System::Drawing::Point(913, -2);
+			this->exit->Margin = System::Windows::Forms::Padding(0);
+			this->exit->Name = L"exit";
+			this->exit->Size = System::Drawing::Size(50, 49);
+			this->exit->TabIndex = 5;
+			this->exit->Text = L"X";
+			this->exit->UseVisualStyleBackColor = false;
+			this->exit->Click += gcnew System::EventHandler(this, &aurorise::exit_Click);
 			// 
-			// textBox1
+			// panel2
 			// 
-			this->textBox1->Location = System::Drawing::Point(10, 200);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(245, 22);
-			this->textBox1->TabIndex = 2;
+			this->panel2->BackColor = System::Drawing::Color::SlateBlue;
+			this->panel2->Location = System::Drawing::Point(10, 328);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(361, 2);
+			this->panel2->TabIndex = 9;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(10, 300);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(245, 22);
-			this->textBox2->TabIndex = 4;
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Montserrat SemiBold", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->textBox2->BackColor = System::Drawing::Color::White;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Montserrat Medium", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->ForeColor = System::Drawing::Color::SlateBlue;
-			this->label3->Location = System::Drawing::Point(3, 248);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(128, 39);
-			this->label3->TabIndex = 3;
-			this->label3->Text = L"Пароль";
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(14, 354);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(117, 34);
-			this->button1->TabIndex = 5;
-			this->button1->Text = L"Войти";
-			this->button1->UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this->button3->Location = System::Drawing::Point(154, 354);
-			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(117, 34);
-			this->button3->TabIndex = 6;
-			this->button3->Text = L"Нет аккаунта\?";
-			this->button3->UseVisualStyleBackColor = true;
+			this->textBox2->ForeColor = System::Drawing::Color::MediumSlateBlue;
+			this->textBox2->Location = System::Drawing::Point(10, 296);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(361, 29);
+			this->textBox2->TabIndex = 2;
 			// 
 			// aurorise
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoScaleDimensions = System::Drawing::SizeF(120, 120);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Dpi;
 			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->BackColor = System::Drawing::Color::White;
-			this->ClientSize = System::Drawing::Size(782, 553);
-			this->Controls->Add(this->tableLayoutPanel1);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(960, 540);
+			this->Controls->Add(this->exit);
+			this->Controls->Add(this->panel1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"aurorise";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"aurorise";
+			this->TopMost = true;
 			this->Load += gcnew System::EventHandler(this, &aurorise::aurorise_Load);
-			this->tableLayoutPanel1->ResumeLayout(false);
-			this->panel2->ResumeLayout(false);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
